ae2ced1405f69dedecf97f2ec6752489155ecb2d
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-05-26 02:24:05 -0700

    title change in app

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Anthony's App</h2>
         </div>
         <p className="App-intro">
           {'This is '}
